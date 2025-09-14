65caf27ab6b590640b8d1e9edd96e186d30d038d
amina <etdchnf@gmail.com>
2025-09-13 14:11:56 -1200

    edditing h calendar

--- src/components/CalendarComp.js
+++ src/components/CalendarComp.js
@@ -10,7 +10,7 @@ import interactionPlugin from "@fullcalendar/interaction";
 export default function CalendarComp({events}){
 const [date,setDate] = useState(new Date())
 return (
-<div className="  bg-white  h-[700px] w-[700px] py-10 px-5  rounded-2xl ">
+<div className="  bg-white  min-h-screen w-[700px] py-10 px-5  rounded-2xl ">
 <h1 className="mb-10 text-2xl font-semibold" >calendar</h1>
  < FullCalendar
  plugins={[dayGridPluging,interactionPlugin]}
