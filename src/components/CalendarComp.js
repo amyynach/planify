20203925492579129a0b22b01a0fbdbbcb0afa6a
amina <etdchnf@gmail.com>
2025-09-13 13:52:32 -1200

    fix calendarComp

--- src/components/CalendarComp.js
+++ src/components/CalendarComp.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 import FullCalendar from "@fullcalendar/react";
 
 import dayGridPluging from "@fullcalendar/daygrid";
-import iteractionPlugin from "@fullcalendar/interaction";
+import interactionPlugin from "@fullcalendar/interaction";
 
 
 export default function CalendarComp({events}){
@@ -13,10 +13,10 @@ return (
 <div className="  bg-white  h-[700px] w-[700px] py-10 px-5  rounded-2xl ">
 <h1 className="mb-10 text-2xl font-semibold" >calendar</h1>
  < FullCalendar
- plugins={[dayGridPluging,iteractionPlugin]}
+ plugins={[dayGridPluging,interactionPlugin]}
  initialView="dayGridMonth"
  events ={events}
-className ="  "
+
 height="auto"
 />
 
